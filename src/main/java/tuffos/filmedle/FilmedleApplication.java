5ebf28b00e648df7ec67cccc7760e13dbeb05a60
leo <leomaxmonte14@gmail.com>
2026-04-25 18:39:06 -0300

    Commit inicial

---
+++ src/main/java/tuffos/filmedle/FilmedleApplication.java
@@ -0,0 +1,13 @@
+package tuffos.filmedle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmedleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmedleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tuffos/filmedle/FilmedleApplicationTests.java
@@ -0,0 +1,13 @@
+package tuffos.filmedle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmedleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
