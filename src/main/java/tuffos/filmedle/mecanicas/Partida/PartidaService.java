398faf707d8ec12173cf8e15ec2a594022ba2d53
leo <leomaxmonte14@gmail.com>
2026-05-03 02:26:35 -0300

    rota de iniciar partida e receber dica

--- src/main/java/tuffos/filmedle/mecanicas/Partida/PartidaService.java
+++ src/main/java/tuffos/filmedle/mecanicas/Partida/PartidaService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import tuffos.filmedle.dados.filme.FilmeService;
 import tuffos.filmedle.mecanicas.Palpite.Palpite;
 import tuffos.filmedle.mecanicas.Palpite.PalpiteService;
 import tuffos.filmedle.mecanicas.Partida.dto.ResponsePartidaDTO;
@@ -15,6 +16,8 @@ public class PartidaService {
     private PartidaRepository partidaRepository;
     @Autowired
     private PalpiteService palpiteService;
+    @Autowired
+    private FilmeService filmeService;
 
     public ResponsePartidaDTO chute(Integer idPartida, Integer idChute) {
 
@@ -28,11 +31,27 @@ public class PartidaService {
 
     }
 
-
     public Partida get(Integer id)  {
         return partidaRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Partida não encontrado"));
     }
     public ResponsePartidaDTO getDTO(Integer id) {return ResponsePartidaDTO.toDTO(get(id));}
+
+    public ResponsePartidaDTO inicia() {
+        Partida partida = new Partida();
+        partida.setFilme(filmeService.getAleatorio());
+        partida = partidaRepository.save(partida);
+        return ResponsePartidaDTO.toDTO(partida);
+    }
+
+    public String[] dica(Integer id) {
+        Partida partida = get(id);
+
+        if (partida.getPalpites().size() >= 5) {
+            return partida.getFilme().getKeywords();
+        } else {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "A partida não tem pelo menos 5 palpites");
+        }
+    }
 
 }
